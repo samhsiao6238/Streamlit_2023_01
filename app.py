db909b1768ad9953ea71074922e6c93af3866c87
samhsiao6238 <gsam6238@gmail.com>
2023-10-16 23:27:28 +0800

    更新

--- app.py
+++ app.py
@@ -1,70 +1,30 @@
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, db
 import json
 
-# 初始化 Firebase 
-def initialize_firebase():
-    try:
-        firebase_admin.get_app()  # 若已初始化，直接返回
-    except ValueError as e:
-        # 先嘗試從 Streamlit Secrets 讀取憑證
-        firebase_config_str = st.secrets.get('FIREBASE_CONFIG_STR', None)
-        if firebase_config_str:
-            try:
-                cred_info = json.loads(firebase_config_str)
-                cred = credentials.Certificate(cred_info)
-            except Exception as e:
-                st.error(f'解析 Streamlit Secrets 上的憑證出錯：{e}')
-                return False
-        else:
-            # 嘗試從本地檔案讀取憑證
-            try:
-                with open('myproject01-be1b7-firebase-adminsdk-1mh85-3ede5c2672.json', 'r') as f:
-                    cred_info = json.load(f)
-                cred = credentials.Certificate(cred_info)
-            except Exception as e:
-                st.error(f'讀取本地 Firebase 憑證出錯，錯誤訊息：{e}')
-                return False
-
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://myproject01-be1b7-default-rtdb.asia-southeast1.firebasedatabase.app/'
-        })
-    return True
-
-# Streamlit 主要內容
 def main():
-    st.title("Streamlit & Firebase 整合範例")
-
-    if not initialize_firebase():
-        st.error("Firebase 初始化失敗！")
-        return
-
-    # 寫入
-    def write_to_firebase():
-        ref = db.reference('data')
-        ref.set({
-            'example_key': 'Hello, Firebase!'
-        })
-
-    # 讀取
-    def read_from_firebase():
-        ref = db.reference('data')
-        data = ref.get()
-        return data
-
-    # 按鈕
-    if st.button("寫入 Firebase"):
-        write_to_firebase()
-        st.success("完成寫入到 Firebase 節點！")
+    st.title("檢查並讀取 Streamlit Secrets")
     
-    # 按鈕
-    if st.button("讀取 Firebase"):
-        data = read_from_firebase()
-        if data:
-            st.write(data)
-        else:
-            st.warning("Firebase 節點上無資料!")
+    # 讀取 db_username 和 db_password
+    db_username = st.secrets.get('db_username', None)
+    db_password = st.secrets.get('db_password', None)
+
+    st.write(f"DB 使用者名稱: {db_username}")
+    st.write(f"DB 密碼: {'*' * len(db_password) if db_password else None}")  # 顯示為星號以保密
+
+    # 讀取 items
+    items = st.secrets.get('items', None)
+    st.write(f"項目清單: {', '.join(items) if items else None}")
+
+    # 讀取 FIREBASE_CONFIG_STR
+    firebase_config_str = st.secrets.get('FIREBASE_CONFIG_STR', None)
+    if firebase_config_str:
+        try:
+            firebase_config = json.loads(firebase_config_str)
+            st.write("Firebase 憑證已讀取，以下是部分信息：")
+            st.write(f"Project ID: {firebase_config.get('project_id')}")
+            st.write(f"Client Email: {firebase_config.get('client_email')}")
+        except json.JSONDecodeError:
+            st.error("讀取 Firebase 憑證時 JSON 解析失敗。")
 
 if __name__ == "__main__":
     main()
