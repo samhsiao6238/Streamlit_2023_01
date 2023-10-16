40e3820dc05ce2667f1dcbdbdafa34bc85bac10a
samhsiao6238 <gsam6238@gmail.com>
2023-10-16 23:45:30 +0800

    更新

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 import streamlit as st
 import json
+import os
 
 def main():
-    st.title("檢查並讀取 Streamlit Secrets")
-    
-    # 讀取 db_username 和 db_password
-    db_username = st.secrets['db_username']
-    db_password = st.secrets['db_password']
+    st.title("檢查並讀取 Streamlit Secrets 或 Firebase JSON 檔")
+
+    # 如果在 Streamlit Sharing 雲端服務上運行
+    if os.environ.get("STREAMLIT_SHARING") == "true":
+        db_username = st.secrets['db_username']
+        db_password = st.secrets['db_password']
+    else:  # 本地運行
+        with open('myproject01-be1b7-firebase-adminsdk-1mh85-3ede5c2672.json', 'r') as f:
+            firebase_data = json.load(f)
+            db_username = firebase_data.get('db_username')  # 這裡的鍵名取決於您的 JSON 檔結構
+            db_password = firebase_data.get('db_password')  # 這裡的鍵名取決於您的 JSON 檔結構
 
     st.write(f"DB 使用者名稱: {db_username}")
     st.write(f"DB 密碼: {'*' * len(db_password) if db_password else None}")  # 顯示為星號以保密
-
-    # # 讀取 FIREBASE_CONFIG_STR
-    # firebase_config_str = st.secrets.get('FIREBASE_CONFIG_STR', None)
-    # if firebase_config_str:
-    #     try:
-    #         firebase_config = json.loads(firebase_config_str)
-    #         st.write("Firebase 憑證已讀取，以下是部分信息：")
-    #         st.write(f"Project ID: {firebase_config.get('project_id')}")
-    #         st.write(f"Client Email: {firebase_config.get('client_email')}")
-    #     except json.JSONDecodeError:
-    #         st.error("讀取 Firebase 憑證時 JSON 解析失敗。")
 
 if __name__ == "__main__":
     main()
