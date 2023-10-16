34d509d65bf73ff638f025c24a502b6c4a889c29
samhsiao6238 <gsam6238@gmail.com>
2023-10-17 00:04:14 +0800

    1111

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ from firebase_admin import credentials, db
 def main():
     st.title("從 Firebase 讀取資料並在 Streamlit 顯示")
 
-    # 初始化 Firebase Admin
-    firebase_config_str = st.secrets['FIREBASE_CONFIG_STR']
-    firebase_config = json.loads(firebase_config_str)
+    # # 初始化 Firebase Admin
+    # firebase_config_str = st.secrets['FIREBASE_CONFIG_STR']
+    # firebase_config = json.loads(firebase_config_str)
         
-    cred = credentials.Certificate(firebase_config)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://myproject01-be1b7-default-rtdb.asia-southeast1.firebasedatabase.app/'
-    })
+    # cred = credentials.Certificate(firebase_config)
+    # firebase_admin.initialize_app(cred, {
+    #     'databaseURL': 'https://myproject01-be1b7-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    # })
         
 
-    # 讀取 Firebase 實時資料庫的資料
-    ref = db.reference('/')
-    data = ref.get()
+    # # 讀取 Firebase 實時資料庫的資料
+    # ref = db.reference('/')
+    # data = ref.get()
 
-    # 在 Streamlit 上顯示資料
-    st.json(data)
+    # # 在 Streamlit 上顯示資料
+    # st.json(data)
 
 if __name__ == "__main__":
     # st.write(st.secrets)
