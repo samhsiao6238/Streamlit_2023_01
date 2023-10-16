d6236ab3ef34e620d5f5bafbbff2ad8e4cffaab3
samhsiao6238 <gsam6238@gmail.com>
2023-10-16 21:52:57 +0800

    更新

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, db
 import os
+import json
 
-st.write("DB 使用者名稱:", st.secrets["db_username"])
-st.write("DB 密碼:", st.secrets["db_password"])
-# 
-st.write("私密的項目清單", st.secrets["secrets"]["items"])
-#
-st.write("從項目清單取出指定的第二個物件:", st.secrets["secrets"]["items"][1])
-#
-st.write("這部分是觀察 os 設定值與 st 設定值是否一致", os.environ["db_username"] == st.secrets["db_username"])
+# 載入
+try:
+    # 嘗試載入權證，假如是在本地運行則會讀取到
+    with open('myproject01-be1b7-firebase-adminsdk-1mh85-3ede5c2672.json', 'r') as f:
+        FIREBASE_CONFIG_STR = json.load(f)
+except FileNotFoundError:
+    # 找不到就從服務器上讀取
+    FIREBASE_CONFIG_STR = st.secrets["FIREBASE_CONFIG_STR"]
+
+# 初始化 Firebase Admin SDK
+if not firebase_admin._apps:
+    #
+    cred = credentials.Certificate(FIREBASE_CONFIG_STR)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://myproject01-be1b7-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+
+# 寫入
+def write_to_firebase():
+    ref = db.reference('data')
+    ref.set({
+        'example_key': 'Hello, Firebase!'
+    })
+
+# 讀取
+def read_from_firebase():
+    ref = db.reference('data')
+    data = ref.get()
+    return data
+
+
+st.title("Streamlit & Firebase 整合範例")
+
+if st.button("寫入 Firebase"):
+    write_to_firebase()
+    st.success("完成寫入到 Firebase 節點！")
+
+if st.button("讀取 Firebase"):
+    data = read_from_firebase()
+    if data:
+        st.write(data)
+    else:
+        st.warning("Firebase 節點上無資料!")
