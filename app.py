77f397aa3076f3c340dd91ce0dfff45872fdb669
samhsiao6238 <gsam6238@gmail.com>
2023-10-16 23:39:55 +0800

    1111

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ def main():
     st.write(f"DB 使用者名稱: {db_username}")
     st.write(f"DB 密碼: {'*' * len(db_password) if db_password else None}")  # 顯示為星號以保密
 
-    # 讀取 FIREBASE_CONFIG_STR
-    firebase_config_str = st.secrets.get('FIREBASE_CONFIG_STR', None)
-    if firebase_config_str:
-        try:
-            firebase_config = json.loads(firebase_config_str)
-            st.write("Firebase 憑證已讀取，以下是部分信息：")
-            st.write(f"Project ID: {firebase_config.get('project_id')}")
-            st.write(f"Client Email: {firebase_config.get('client_email')}")
-        except json.JSONDecodeError:
-            st.error("讀取 Firebase 憑證時 JSON 解析失敗。")
+    # # 讀取 FIREBASE_CONFIG_STR
+    # firebase_config_str = st.secrets.get('FIREBASE_CONFIG_STR', None)
+    # if firebase_config_str:
+    #     try:
+    #         firebase_config = json.loads(firebase_config_str)
+    #         st.write("Firebase 憑證已讀取，以下是部分信息：")
+    #         st.write(f"Project ID: {firebase_config.get('project_id')}")
+    #         st.write(f"Client Email: {firebase_config.get('client_email')}")
+    #     except json.JSONDecodeError:
+    #         st.error("讀取 Firebase 憑證時 JSON 解析失敗。")
 
 if __name__ == "__main__":
     main()
